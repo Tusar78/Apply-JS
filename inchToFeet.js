cd482fb23b8017e80cebd9ac100cc33141a3c650
Tusar <tusarahmed141@gmail.com>
2022-02-01 10:32:57 +0600

    Inch to foot converter

--- inchToFeet.js
+++ inchToFeet.js
@@ -1,2 +1,8 @@
 // Inch to feet converter
-
+let inches = prompt('Inches', '0');
+const inchToFoot = (num) => {
+    let foot = num / 12;
+    return `${num} inches = ${foot} foot.`;
+}
+let getFoot = inchToFoot(inches);
+alert(getFoot);
